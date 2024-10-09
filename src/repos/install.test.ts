40bb0870547bffd2e3c3b3bafc1b5bd3152a34fd
C. T. J. Lewis <lewis@spellcraft.org>
2024-10-09 01:12:31 -0700

    chore: test `bun install` in CI

---
+++ src/repos/install.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from "bun:test"
+import { Glob } from "bun"
+import { join } from "path"
+
+const reposDir = "./repos"
+
+const skip = ["mini-css-extract-plugin.git"]
+
+describe("bun install", () => {
+  const glob = new Glob("*")
+  const repos = Array.from(glob.scanSync({ cwd: reposDir, onlyFiles: false }))
+
+  let i = 0
+  for (const repo of repos) {
+    if (skip.includes(repo)) {
+      i++
+      continue
+    }
+
+    it(`${repo}`, async () => {
+      console.log(i++, "of", repos.length)
+
+      const repoPath = join(reposDir, repo)
+      const result = await Bun.$`cd ${repoPath} && bun i`
+      expect(result.exitCode).toBe(0)
+    })
+  }
+})
